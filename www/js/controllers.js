0a36c8875fb30896059fe6a6c222fe7fea74a126
Alexis ZRIHEN <alexis.zrihen@hotmail.fr>
2015-05-05 12:52:58 +0200

    map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,128 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $scope.closeLogin();
+        }, 1000);
+    };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+    $scope.playlists = [{
+        title: 'Reggae',
+        id: 1
+    }, {
+        title: 'Chill',
+        id: 2
+    }, {
+        title: 'Dubstep',
+        id: 3
+    }, {
+        title: 'Indie',
+        id: 4
+    }, {
+        title: 'Rap',
+        id: 5
+    }, {
+        title: 'Cowbell',
+        id: 6
+    }];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PlaylistCtrl', function($scope, $stateParams) {})
+
+.controller('MapCtrl', function($scope) {
+
+    var cities = [{
+        city: 'Toronto',
+        desc: 'This is the best city in the world!',
+        lat: 43.7000,
+        long: -79.4000
+    }, {
+        city: 'New York',
+        desc: 'This city is aiiiiite!',
+        lat: 40.6700,
+        long: -73.9400
+    }, {
+        city: 'Chicago',
+        desc: 'This is the second best city in the world!',
+        lat: 41.8819,
+        long: -87.6278
+    }, {
+        city: 'Los Angeles',
+        desc: 'This city is live!',
+        lat: 34.0500,
+        long: -118.2500
+    }, {
+        city: 'Las Vegas',
+        desc: 'Sin City...\'nuff said!',
+        lat: 36.0800,
+        long: -115.1522
+    }];
+
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var createMarker = function(info) {
+
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+
+        $scope.markers.push(marker);
+
+    }
+
+    for (i = 0; i < cities.length; i++) {
+        createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker) {
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
 });
